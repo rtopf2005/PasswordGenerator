1832ff5719d08dde13710b5dece1d8d74af6f512
Ryan Topf <rtopf2005@gmail.com>
2025-02-20 17:36:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+Console.WriteLine("Hello user, please enter how many characters long you would like your password: ");
+
+int a = -1;
+
+if (int.TryParse(Console.ReadLine(), out a) && a>0)
+{
+
+        Random random = new Random();
+
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#@!$%^&*-_";
+        string s = new string(Enumerable.Repeat(chars, a).Select(s => s[random.Next(s.Length)]).ToArray());
+        Console.WriteLine(s);
+}
+else
+{
+    Console.WriteLine("You entered an invalid password length. Please run this program again.");
+}
